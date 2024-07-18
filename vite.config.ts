eb9117d91370683cfd2689a43d643ecb8121e2b3
Even Luiv <luiveven7@gmail.com>
2024-07-18 11:27:08 +0300

    update vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import history from 'connect-history-api-fallback';
+import type { Plugin } from 'vite';
 
-// https://vitejs.dev/config/
+const historyFallbackMiddleware: Plugin = {
+  name: 'history-fallback-middleware',
+  configureServer(server) {
+    const middleware = history({
+      index: '/index.html',
+      verbose: true,
+    }) as unknown as (req: unknown, res: unknown, next: (err?: unknown) => void) => void;
+
+    server.middlewares.use(middleware);
+  },
+};
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), historyFallbackMiddleware],
+});
